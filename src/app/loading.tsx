92cded718db7b8b7a8c0ac6217b87f3563b7cc6f
Ifrad <ifradm0953@gmail.com>
2025-06-15 22:46:57 -0400

    feat: Implement basic loading and error UIs, remove conflicting favicon, add pnpm workspace config

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React from 'react';
+export default function Loading() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center', fontSize: '24px' }}>
+      Loading...
+    </div>
+  );
+}
